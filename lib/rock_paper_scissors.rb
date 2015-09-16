e101644bbeb9946917350970f8047b0a3b0f154f
Ronando Long <ronando.long@gmail.com>
2015-09-16 12:56:46 -0700

    Killed it, started over. 1st spec passes, results in true for rock:rock tie.

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,31 +2,12 @@ class String
   define_method(:beats?) do |player_2|
     player_1 = self
 
-    if player_1 == "rock"
-      if (player_2 == "scissors")
-        "Rock crushes Scissors, Player 1 wins."
-      elsif (player_2 == "paper")
-        "Paper smothers rock, Player 2 wins."
-      else
-        false
-      end
-    elsif (player_1 == "scissors")
-      if (player_2 == "rock")
-        "Rock crushes Scissors, Player 2 wins."
-      elsif (player_2 == "paper")
-        "Scissors cuts paper, Player 1 wins."
-      else
-        false
-      end
-    elsif (player_1 == "paper")
-      if (player_2 == "rock")
-        "Paper smothers rock, Player 1 wins."
-      elsif (player_2 == "scissors")
-        "Scissors cuts paper, Player 2 wins."
-      end
+    if (player_1 == "rock") && (player_2 == "rock")
+      "Result is a tie, try again."
     else
       false
     end
+
   end
 
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -2,27 +2,31 @@ require('rspec')
 require('rock_paper_scissors')
 
 describe("String#rock_paper_scissors") do
-  it("returns true if rock is the object and scissors is the argument") do
-    expect("rock".beats?("scissors")).to(eq("Rock crushes Scissors, Player 1 wins."))
+  it("returns tie if rock is both object and argument") do
+    expect("rock".beats?("rock")).to(eq("Result is a tie, try again."))
   end
 
-  it("returns true if rock is the object and paper is the argument") do
-    expect("rock".beats?("paper")).to(eq("Paper smothers rock, Player 2 wins."))
-  end
-
-  it("returns true if scissors is the object and rock is the argument") do
-    expect("scissors".beats?("rock")).to(eq("Rock crushes Scissors, Player 2 wins."))
-  end
-
-  it("returns true if scissors is the object and paper is the argument") do
-    expect("scissors".beats?("paper")).to(eq("Scissors cuts paper, Player 1 wins."))
-  end
-
-  it("returns true if paper is the object and rock is the argument") do
-    expect("paper".beats?("rock")).to(eq("Paper smothers rock, Player 1 wins."))
-  end
-
-  it("returns true if paper is the object and scissors is the argument") do
-    expect("paper".beats?("scissors")).to(eq("Scissors cuts paper, Player 2 wins."))
-  end
+  # it("returns true if rock is the object and paper is the argument") do
+  #   expect("rock".beats?("paper")).to(eq("Paper smothers rock, Player 2 wins."))
+  # end
+  #
+  # it("returns true if scissors is the object and rock is the argument") do
+  #   expect("scissors".beats?("rock")).to(eq("Rock crushes Scissors, Player 2 wins."))
+  # end
+  #
+  # it("returns true if scissors is the object and paper is the argument") do
+  #   expect("scissors".beats?("paper")).to(eq("Scissors cuts paper, Player 1 wins."))
+  # end
+  #
+  # it("returns true if paper is the object and rock is the argument") do
+  #   expect("paper".beats?("rock")).to(eq("Paper smothers rock, Player 1 wins."))
+  # end
+  #
+  # it("returns true if paper is the object and scissors is the argument") do
+  #   expect("paper".beats?("scissors")).to(eq("Scissors cuts paper, Player 2 wins."))
+  # end
+  #
+  # it("resolves a tie if object and argument are the same") do
+  #   expect("rock".beats?("rock")).to(eq("Result is a tie. Try again."))
+  # end
 end
