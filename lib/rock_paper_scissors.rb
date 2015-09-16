85ef8803c00db2bbf4ed449ab64af53025e5f7ed
Ronando Long <ronando.long@gmail.com>
2015-09-16 10:29:17 -0700

    2nd spec fails as expected, paper argument is to smother rock object.

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,7 +2,7 @@ class String
   define_method(:beats?) do |player_2|
     player_1 = self
 
-    if player_1 = "rock"
+    if player_1 == "rock"
       if (player_2 == "scissors")
         "Rock crushes Scissors, Player 1 wins."
       else
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('rock_paper_scissors')
 
 describe("String#rock_paper_scissors") do
-    it("returns true if rock is the object and scissors is the argument") do
-      expect("rock".beats?("scissors")).to(eq("Rock crushes Scissors, Player 1 wins."))
+  it("returns true if rock is the object and scissors is the argument") do
+    expect("rock".beats?("scissors")).to(eq("Rock crushes Scissors, Player 1 wins."))
+  end
+
+  it("returns true if rock is the object and paper is the argument") do
+    expect("rock".beats?("paper")).to(eq("Paper smothers rock, Player 2 wins."))
   end
 end
