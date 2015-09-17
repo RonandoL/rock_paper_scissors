58e4c85bb36c4563e734f387d4a997e3f17b7e1b
Ronando Long <ronando.long@gmail.com>
2015-09-16 21:14:37 -0700

    It will successfully handle all entries and push out the correct winner. FTW.

--- app.rb
+++ app.rb
@@ -8,8 +8,8 @@ get('/') do
 end
 
 get('/results') do  # user goes to this URL
-  @player_1 = params.fetch('player_1').capitalize
-  @player_2 = params.fetch('player_2').capitalize
-  @player = params.fetch('player_1').beats?('player_2')  # data from form name field
+  @player_1 = params.fetch('player_1').capitalize!
+  @player_2 = params.fetch('player_2').capitalize!
+  @player = @player_1.beats?(@player_2)  # data from form name field
   erb(:results_file)  # this is the filename
 end
--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,23 +1,23 @@
 class String
   define_method(:beats?) do |player_2|
     player_1 = self
-    player_1.downcase!
-    player_2.downcase!
+    player_1.capitalize!
+    player_2.capitalize!
 
-    if (player_1 == "rock") && (player_2 == "rock") or
-       (player_1 == "paper") && (player_2 == "paper") or
-       (player_1 == "scissors") && (player_2 == "scissors")
+    if (player_1 == "Rock") && (player_2 == "Rock") or
+       (player_1 == "Paper") && (player_2 == "Paper") or
+       (player_1 == "Scissors") && (player_2 == "Scissors")
       "Result is a tie, try again."
-    elsif (player_1 == "rock") && (player_2 == "paper") or
-          (player_1 == "paper") && (player_2 == "scissors") or
-          (player_1 == "scissors") && (player_2 == "rock")
+    elsif (player_1 == "Rock") && (player_2 == "Paper") or
+          (player_1 == "Paper") && (player_2 == "Scissors") or
+          (player_1 == "Scissors") && (player_2 == "Rock")
       "Player 2 wins."
-    elsif (player_1 == "rock") && (player_2 == "scissors") or
-          (player_1 == "paper") && (player_2 == "rock") or
-          (player_1 == "scissors") && (player_2 == "paper")
+    elsif (player_1 == "Rock") && (player_2 == "Scissors") or
+          (player_1 == "Paper") && (player_2 == "Rock") or
+          (player_1 == "Scissors") && (player_2 == "Paper")
       "Player 1 wins."
     else
-      "Please just type 'Rock', 'Paper' or 'scissors'."
+      "Please just type 'Rock', 'Paper' or 'Scissors'."
     end
 
   end
